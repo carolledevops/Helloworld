528f4c6e918b543e45d3c2ac69e108a80bc7d969
carolledevops <138341326+carolledevops@users.noreply.github.com>
2023-07-04 01:01:46 -0500

    Update servers.tf

--- infrastructure/servers.tf
+++ infrastructure/servers.tf
@@ -1,12 +1,10 @@
-data "google_compute_zones" "available" {
-    region = var.region
-}
 
 resource "google_compute_address" "jenkins" {
   name = var.address_name
 }
 
-resource "google_compute_instance" "hello_vm0" {
+
+resource "google_compute_instance" "hello_vm" {
   name         = var.instance_name
   machine_type = var.machine_type
   zone = var.zone
@@ -22,6 +20,23 @@ resource "google_compute_instance" "hello_vm0" {
     access_config {
       nat_ip = google_compute_address.jenkins.address
     }
+  }
+}
+
+resource "google_compute_instance" "hello_node" {
+  name         = var.instance_name
+  machine_type = var.machine_type
+  zone = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.hello_subnet.id
+    
   }
 }
 
